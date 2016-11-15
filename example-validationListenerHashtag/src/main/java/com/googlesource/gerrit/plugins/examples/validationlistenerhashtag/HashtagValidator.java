db165946cefdde6138811124a4bbf91251a5e67f
Martin Fick <mogulguy10@gmail.com>
2016-11-14 20:04:11 -0700

    example-validationListener*
    Change-Id: I016396287b6fd62f83fdd1b1bb577d191b266516

---
+++ example-validationListenerHashtag/src/main/java/com/googlesource/gerrit/plugins/examples/validationlistenerhashtag/HashtagValidator.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.examples.validationlistenerhashtag;
+
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.validators.HashtagValidationListener;
+import com.google.gerrit.server.validators.ValidationException;
+
+import java.util.Set;
+
+public class HashtagValidator implements HashtagValidationListener {
+
+  @Override
+  public void validateHashtags(Change change, Set<String> toAdd, Set<String> toRemove)
+      throws ValidationException {
+    if (change.getProject().get().equals("plugins/example-validationListenerHashtag")) {
+      if (toAdd.size() > 0) {
+        for (String hashtag : toAdd) {
+          if (!hashtag.startsWith("example-validationListenerHashtag-")) {
+            throw new ValidationException("Invalid example-validationListenerHashtag hashtag: " + hashtag);
+          }
+        }
+      }
+
+      if (toRemove.size() > 0) {
+        throw new ValidationException("Cannot remove example-validationListenerHashtag hashtags");
+      }
+    }
+  }
+}
---
+++ example-validationListenerRefOperation/src/main/java/com/googlesource/gerrit/plugins/examples/validationlistenerrefoperation/RefOperationValidationExample.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.examples.validationlistenerrefoperation;
+
+import com.google.common.collect.Lists;
+import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.gerrit.server.events.RefReceivedEvent;
+import com.google.gerrit.server.git.validators.RefOperationValidationListener;
+import com.google.gerrit.server.git.validators.ValidationMessage;
+import com.google.gerrit.server.validators.ValidationException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RefOperationValidationExample implements
+    RefOperationValidationListener {
+
+  @Override
+  public List<ValidationMessage> onRefOperation(RefReceivedEvent event)
+      throws ValidationException {
+    ArrayList<ValidationMessage> messages = Lists.newArrayList();
+    if (event.command.getRefName()
+        .startsWith(RefNames.REFS_HEADS + "protected-")
+        && !event.user.getCapabilities().canAdministrateServer()) {
+      throw new ValidationException(String.format(
+          "Operation %s on %s branch in project %s is not valid!",
+          event.command.getType(),
+          event.command.getRefName(),
+          event.project.getName()));
+    }
+    return messages;
+  }
+}
