8d725af82bb7be71832d7cb0e1e5645971d4aaed
Adithya Chakilam <achakila@codeaurora.org>
2020-10-22 11:48:42 -0500

    Update example-changeQueryAttributes
    Replace the deprecated ChangeAttributeFactory class with the
    ChangePluginDefinedInfoFactory class.
    
    Introduced in change:
        https://gerrit-review.googlesource.com/c/gerrit/+/280758
    
    Change-Id: I188f3f40eadcaf70aab74c8f1fee96797dd2d540

--- example-changeQueryAttributes/src/main/java/com/googlesource/gerrit/plugins/examples/changequeryattributes/Module.java
+++ example-changeQueryAttributes/src/main/java/com/googlesource/gerrit/plugins/examples/changequeryattributes/Module.java
@@ -15,14 +15,33 @@
 package com.googlesource.gerrit.plugins.examples.changequeryattributes;
 
 import com.google.gerrit.extensions.annotations.Exports;
-import com.google.gerrit.server.change.ChangeAttributeFactory;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.DynamicOptions;
+import com.google.gerrit.server.change.ChangePluginDefinedInfoFactory;
+import com.google.gerrit.server.restapi.change.GetChange;
+import com.google.gerrit.server.restapi.change.QueryChanges;
+import com.google.gerrit.sshd.commands.Query;
 import com.google.inject.AbstractModule;
 
 public class Module extends AbstractModule {
   @Override
   protected void configure() {
-    bind(ChangeAttributeFactory.class)
-        .annotatedWith(Exports.named("example-changeQueryAttributes"))
-        .to(AttributeFactory.class);
+    // Register attribute factory.
+    DynamicSet.bind(binder(), ChangePluginDefinedInfoFactory.class).to(AttributeFactory.class);
+
+    // Register options for GET /changes/X/change and /changes/X/detail.
+    bind(DynamicOptions.DynamicBean.class)
+        .annotatedWith(Exports.named(GetChange.class))
+        .to(AttributeFactory.MyChangeOptions.class);
+
+    // Register options for GET /changes/?q=...
+    bind(DynamicOptions.DynamicBean.class)
+        .annotatedWith(Exports.named(QueryChanges.class))
+        .to(AttributeFactory.MyChangeOptions.class);
+
+    // Register options for ssh gerrit query.
+    bind(DynamicOptions.DynamicBean.class)
+        .annotatedWith(Exports.named(Query.class))
+        .to(AttributeFactory.MyChangeOptions.class);
   }
 }
